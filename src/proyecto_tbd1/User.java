4880dccaaa3dab68d267d3e557a540f42d331f88
Víctor Romero <virn2005@gmail.com>
2024-03-04 23:59:23 -0600

    User Class Created

---
+++ src/proyecto_tbd1/User.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_tbd1;
+
+/**
+ *
+ * @author Victor
+ */
+public class User {
+
+    protected String username;
+    protected String password;
+
+    public User() {
+    }
+
+    public User(String username, String password, int edad) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
